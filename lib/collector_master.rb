d4bc288154b0cd934ddb267165341ca8154f0c38
wangxiangyu <wangxiangyu@baidu.com>
2014-04-03 15:59:47 +0800

    update

--- lib/collector_master.rb
+++ lib/collector_master.rb
@@ -22,11 +22,11 @@ module CollectorMaster
         attr_reader :logger
         def run
             begin
-            init_task
-            register_collector
-            register_task
-            remove_dead_collector
-            check_task_assign
+                init_task
+                register_collector
+                register_task
+                remove_dead_collector
+                check_task_assign
             rescue => e
                 logger.error("Error in collect master: #{e.message} #{e.backtrace}")
             end
@@ -40,18 +40,23 @@ module CollectorMaster
             end
         end
         def assign_task
-            collector_num=@collectors.size
-            return if collector_num==0
-            task_for_each_collector=512/collector_num+1
-            count=0
-            collector_ips=@collectors.keys.sort
-            collector_ips.each do |ip|
-                collector_client=@collectors[ip]
-                data={}
-                collector_client.start_index=data["start_index"]=task_for_each_collector*count
-                collector_client.end_index=data["end_index"]=(task_for_each_collector*(count+1)-1)
-                count=count+1
-                nats.publish("collector_task_#{ip}",data)
+            begin
+                collector_num=@collectors.size
+                return if collector_num==0
+                task_for_each_collector=512/collector_num+1
+                count=0
+                collector_ips=@collectors.keys.sort
+                collector_ips.each do |ip|
+                    collector_client=@collectors[ip]
+                    data={}
+                    collector_client.start_index=data["start_index"]=task_for_each_collector*count
+                    collector_client.end_index=data["end_index"]=(task_for_each_collector*(count+1)-1)
+                    count=count+1
+                    nats.publish("collector_task_#{ip}",data)
+                end
+                logger.info("task assign")
+            rescue => e
+                logger.error("Error in assign task: #{e.message} #{e.backtrace}")
             end
         end
         def delete_collector(ip)
@@ -69,45 +74,61 @@ module CollectorMaster
             Time.now.to_i-time.to_i>10
         end
         def remove_dead_collector
-            EM::PeriodicTimer.new(10) do
-                @collectors.each do |ip,collector_client|
-                    if timeout?(collector_client.update_time)
-                        logger.info("remove dead collector #{ip}")
-                        delete_collector(ip)
+            begin
+                EM::PeriodicTimer.new(10) do
+                    @collectors.each do |ip,collector_client|
+                        if timeout?(collector_client.update_time)
+                            logger.info("remove dead collector #{ip}")
+                            delete_collector(ip)
+                        end
                     end
                 end
+            rescue => e
+                logger.error("Error in remove dead collector: #{e.message} #{e.backtrace}")
             end
         end
         def register_collector
-            nats.subscribe("collector_register") do |message|
-                ip=message.data["ip"]
-                if !@collectors.has_key?(ip)
-                    add_collector(ip)
-                else
-                    update_collector(ip)
+            begin
+                nats.subscribe("collector_register") do |message|
+                    ip=message.data["ip"]
+                    if !@collectors.has_key?(ip)
+                        add_collector(ip)
+                    else
+                        update_collector(ip)
+                    end
                 end
+            rescue => e
+                logger.error("Error in register collector: #{e.message} #{e.backtrace}")
             end
         end
         
         def check_task_assign
-            EM::PeriodicTimer.new(10) do
-                all_assign=true
-                (0..255).each do |index|
-                   all_assign=false if @tasks[index].to_i < Time.now.to_i-15
+            begin
+                EM::PeriodicTimer.new(10) do
+                    all_assign=true
+                    (0..255).each do |index|
+                       all_assign=false if @tasks[index].to_i < Time.now.to_i-15
+                    end
+                    unless all_assign
+                        logger.info("task re assign")
+                        assign_task unless all_assign           
+                    end
                 end
-                unless all_assign
-                    logger.info("task re assign")
-                    assign_task unless all_assign           
-                end
+            rescue => e
+                logger.error("Error in check task assign: #{e.message} #{e.backtrace}")
             end
         end
 
         def register_task
-            nats.subscribe("task_register") do |message|
-                index=message.data["index"]
-                index.each do |i|
-                    @tasks[i]=Time.now.to_i
+            begin
+                nats.subscribe("task_register") do |message|
+                    index=message.data["index"]
+                    index.each do |i|
+                        @tasks[i]=Time.now.to_i
+                    end
                 end
+            rescue => e
+                logger.error("Error in register task: #{e.message} #{e.backtrace}")
             end
         end
         class CollectorClient
